1e982d857eb5216dce039d028f6edd4cfd10b81c
arthursantos94466-star <arthursantos94466@gmail.com>
2026-05-23 02:49:34 -0300

    Remove external script loading from Aimbot button
    Removed a script loading line from the Aimbot button callback.

--- Hashira.lua
+++ Hashira.lua
@@ -54,7 +54,7 @@ local ExtraTab = Window:CreateTab("Extra",4483362458)
 CombatTab:CreateButton({
     Name = "Aimbot (External)", -- Corrigido: Removido o ponto e vírgula daqui
     Callback = function()
-        loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Phantom-Hub-BEST-AIMBOT-194105"))()
+        loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Aimbot-universal-no-key-94177"))()
     end
 })
 
